950b4628a56b5c9989ae691279346930bc524323
Horockey <rassudov99@gmail.com>
2022-11-17 22:38:09 +0300

    refactor: Lab4 better example

--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -4,7 +4,7 @@
 	{
 		static void Main(string[] args)
 		{
-			var en = new EnumerableSolutionClass("isdjb 56 321 sdo 1 032 wcmd");
+			var en = new EnumerableSolutionClass("isdjb 56 321 sdo 1 032 1 wcmd");
 			var dict1 = new Dictionary<string, bool>();
 			foreach(var el in en)
 			{
@@ -19,7 +19,7 @@
 
 			Console.Write("\n============\n");
 
-			var enf = new EnumFuncSolutionClass("isdjb 56 321 sdo 1 032 wcmd");
+			var enf = new EnumFuncSolutionClass("isdjb 56 321 sdo 1 032 1 wcmd");
 			var dict2 = new Dictionary<string, bool>();
 			foreach(var el in enf.Range(0, enf.Size))
 			{
